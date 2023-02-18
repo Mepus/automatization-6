b99461843c670cf75207ec2141adc31be2bf0fe7
Mepus <mepurisvili@gmail.com>
2023-02-18 11:37:17 +0300

    Переделка

--- homework4.py
+++ homework4.py
@@ -10,53 +10,60 @@ class Register:
     def browser(self):
         driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-def input_first_name(self,locator:str, txt:str):  
+
+def input_field(self,locator:str, txt:str):  
     driver.find_element(By.CSS_SELECTOR, locator).click()
-    first_name = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
-    return first_name
+    el = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
+    return el
 
 
-def input_last_name(self,locator:str, txt:str):  
-    driver.find_element(By.CSS_SELECTOR, locator).click()
-    last_name = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
-    return last_name
+# def input_first_name(self,locator:str, txt:str):  
+#     driver.find_element(By.CSS_SELECTOR, locator).click()
+#     first_name = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
+#     return first_name
 
 
-def input_address(self,locator:str, txt:str):  
-    driver.find_element(By.CSS_SELECTOR, locator).click()
-    address = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
-    return address
-
-def input_zip_code(self,locator:str, txt:str):  
-    driver.find_element(By.CSS_SELECTOR, locator).click()
-    zip_code = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
-    return zip_code
+# def input_last_name(self,locator:str, txt:str):  
+#     driver.find_element(By.CSS_SELECTOR, locator).click()
+#     last_name = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
+#     return last_name
 
 
-def input_city(self,locator:str, txt:str):  
-    driver.find_element(By.CSS_SELECTOR, locator).click()
-    city = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
-    return city
+# def input_address(self,locator:str, txt:str):  
+#     driver.find_element(By.CSS_SELECTOR, locator).click()
+#     address = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
+#     return address
+
+# def input_zip_code(self,locator:str, txt:str):  
+#     driver.find_element(By.CSS_SELECTOR, locator).click()
+#     zip_code = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
+#     return zip_code
 
 
-def input_country(self,locator:str, txt:str):  
-    driver.find_element(By.CSS_SELECTOR, locator).click()
-    country = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
-    return country
+# def input_city(self,locator:str, txt:str):  
+#     driver.find_element(By.CSS_SELECTOR, locator).click()
+#     city = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
+#     return city
 
 
-def input_job_position(self,locator:str, txt:str):  
-    driver.find_element(By.CSS_SELECTOR, locator).click()
-    job_position = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
-    return job_position
+# def input_country(self,locator:str, txt:str):  
+#     driver.find_element(By.CSS_SELECTOR, locator).click()
+#     country = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
+#     return country
 
-def input_company(self,locator:str, txt:str):  
-    driver.find_element(By.CSS_SELECTOR, locator).click()
-    company = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
-    return company
 
-def button_click(self,button):
-    driver.find_element(By.CSS_SELECTOR, button).click()
+# def input_job_position(self,locator:str, txt:str):  
+#     driver.find_element(By.CSS_SELECTOR, locator).click()
+#     job_position = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
+#     return job_position
+
+# def input_company(self,locator:str, txt:str):  
+#     driver.find_element(By.CSS_SELECTOR, locator).click()
+#     company = driver.find_element(By.CSS_SELECTOR, locator).send_keys(txt)
+#     return company
+
+# def button_click(self,button):
+#     driver.find_element(By.CSS_SELECTOR, button).click()
 
 
 
